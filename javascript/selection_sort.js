ca1d668fb3f250325886634a049b45ce6c3d3de4
Kyle Wehrung <kyle.wehrung@gmail.com>
2023-04-27 23:16:24 -0600

    sorting, fun!

--- javascript/selection_sort.js
+++ javascript/selection_sort.js
@@ -1,6 +1,20 @@
 function selectionSort(arr) {
-  // type your code here
+  const sorty = []
+
+  while (arr.length > 0) {
+    const min = Math.min(...arr)
+    const index = arr.indexOf(min)
+
+    sorty.push(min)
+    arr.splice(index, 1)
+  }
+  
+ return sorty
+
 }
+
+
+
 
 if (require.main === module) {
   // add your own tests in here
@@ -10,14 +24,36 @@ if (require.main === module) {
   console.log("");
 
   // BENCHMARK HERE, and print the average runtime
-  const longInput = [];
+  const longInput = []
 
   for (let i = 0; i < 100; ++i) {
-    longInput.push(Math.random());
+    longInput.push(Math.random())
   }
+  
+  const startTime = Date.now()
+  
+  for (let i = 0; i < 1000; ++i) {
+    selectionSort([2, 1])
+    selectionSort(longInput)
+  }
+  
+  const avgTime = (Date.now() - startTime) / 2000
+
+
+  console.log(avgTime)
+
+
+
 }
 
 module.exports = selectionSort;
 
 // Please add your pseudocode to this file
+  // create new array to push things into
+  // find minimum of array with Math.min
+  // find index of minimum num with arr.indexOf
+  // push minimum into new array
+  // remove minimum element from og array
+
+
 // And a written explanation of your solution
